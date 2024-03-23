7c23126038591f9fe6b3217a7066f88da2b76c3c
LucidTyper <84922760+lucidtyper@users.noreply.github.com>
2024-03-23 19:29:14 +0100

    post install script

---
+++ ArchPostInstall.sh
@@ -0,0 +1,12 @@
+pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si # copied from https://github.com/Jguer/yay
+echo -e "blacklist pcspkr\nblacklist snd_pcsp" > sudo /etc/modprobe.d/nobeep.conf
+yay -S envycontrol pamac-all ibus-pinyin ntfs-3g touchegg timeshift extension-manager microsoft-edge-stable-bin noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra dccutil xournalpp ncdu yt-dlp dropbox megasync okular cpupower cpupower-gui btop fzf ncdu vim --noconfirm
+sudo systemctl enable --now cronie
+sudo systemctl enable --now bluetooth
+sudo systemctl enable --now touchegg
+sudo timeshift-gtk &
+sudo envycontrol -s hybrid --rtd3 3
+
+echo -e "\n\nYou should now reboot and, if everything went along the rails, have the first automatic backup ten minutes after login in. The system WILL REBOOT in 60 seconds."
+sleep 60
+reboot
